1dca6b85c054d0141d3351f69113da301c1761a0
enji7 <enji.systems@gmail.com>
2021-05-15 22:03:29 +0200

    exceptions, colonize

--- README.md
+++ README.md
@@ -7,6 +7,11 @@ Quick tour through some JavaScript features.
 * install [Node.js](https://nodejs.org/en/)
 * on the command line: `node <js file>`
 
+## IDE hints
+
+* [Visual Studio Code](https://code.visualstudio.com/) works great for JavaScript development
+  * the [Colonize](https://marketplace.visualstudio.com/items?itemName=vmsynkov.colonize) extension makes it easier to add semicolons at line ends
+  * semicolons can also be added through he built-in code formatter (Ctrl+Shift-I on Linux) if the "JavaScript > Format: Semicolons" setting is enabled
 
 ## References
 
---
+++ exceptions.js
@@ -0,0 +1,55 @@
+// throw and catch the built-in Error object;
+// see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Error;
+// can be extended to add user-defined exceptions (see below)
+try {
+    throw new Error("error message");
+}
+catch (e) {
+    // Error has certain pre-defined properties:
+    console.log(`name: ${e.name}, message: ${e.message}`);
+    // also logs the stack trace:
+    console.log(e);
+}
+finally {
+    console.log("finally is always executed");
+}
+
+// you can throw just about anything (even though throwing dedicated exception objects is more useful)
+try {
+    throw "thrown string";
+}
+catch (e) {
+    console.log(e);
+}
+
+/** Example for a custom error that adds an error code. */
+class ErrorWithCode extends Error {
+
+    constructor(message, code) {
+        super(message);
+        this.code = code;
+    }
+
+}
+
+/** Example for a custom error that adds an error reason. */
+class ErrorWithReason extends Error {
+
+    constructor(message, reason) {
+        super(message);
+        this.reason = reason;
+    }
+
+}
+
+try {
+    throw new ErrorWithCode("ce1", "E-42");
+} catch (e) {
+    console.log(e);   
+}
+
+try {
+    throw new ErrorWithReason("ce1", "some arbitrary reason object");
+} catch (e) {
+    console.log(e);   
+}
